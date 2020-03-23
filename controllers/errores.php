eaa76aa9e9de9f9c30814e43868ade5acb8694b4
Nathaly Palencia <nathaly@Nathalys-Mac-mini.local>
2020-03-22 19:36:11 -0600

    Mensaje de error bonito

--- controllers/errores.php
+++ controllers/errores.php
@@ -3,7 +3,7 @@
 
         function __construct(){
             parent::__construct();
-            $this->view->mensaje="Hubo un error en la solicitud o no existe la página";
+            $this->view->mensaje="Hubo un error en la solicitud o no existe la página.";
             $this->view->render('error/index');
             //echo "<p>Error al cargar recurso</p>";
         }
